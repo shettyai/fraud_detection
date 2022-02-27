1623299f0339745b90d234e01fa96c274759bf52
shettyai <aishwaryakkshetty@gmail.com>
2022-02-27 10:11:10 +0530

    Added sum and mul functions

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,12 @@
 print("adding all data processing codes")
 print("hello all")
+
+a=5
+b=7
+prinT("sum function")
+def sum(a,b):
+    return a+b
+
+print("Subtraction")
+def mul(a,b):
+    return a*b
